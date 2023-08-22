199663e56a34d1e66b4d02a62c87998f05a071bd
Sea-zone <jseazone7@gmail.com>
2023-08-22 20:35:25 +0545

    Javascript Updated

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
-let i = 0;
-let a = setInterval(displaymessage);
-
-function displaymessage() {
-  if (i !== 143) {
-    document.getElementById("demo").innerHTML = `<h1>${i}</h1>`;
-    i++;
-    return;
+let counts = setInterval(updated);
+let upto = 0;
+function updated() {
+  let count = document.getElementById("counter1");
+  count.innerHTML = ++upto;
+  if (upto === 142) {
+    clearInterval(counts);
   }
-  clearInterval(a);
+}
+
+let counts2 = setInterval(updated2, 25);
+let upto2 = 0;
+function updated2() {
+  let count = document.getElementById("counter2");
+  console.log(counts2);
+  count.innerText = ++upto2;
+  if (upto2 === 28) {
+    clearInterval(counts2);
+  }
+}
+
+let counts3 = setInterval(updated3, 15);
+let upto3 = 0;
+function updated3() {
+  let count = document.getElementById("counter3");
+  count.innerHTML = ++upto3;
+  if (upto3 === 53) {
+    clearInterval(counts3);
+  }
+}
+
+let counts4 = setInterval(updated4, 10);
+let upto4 = 0;
+function updated4() {
+  let count = document.getElementById("counter4");
+  count.innerHTML = ++upto4;
+  if (upto4 === 90) {
+    clearInterval(counts4);
+  }
 }
